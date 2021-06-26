bd33ab15f28f73a484ac9d429929f93d30b667ba
lsefiane <lyes.sefiane@gmail.com>
2021-06-25 20:43:30 -0400

    ~ Subscriber Service Setup

---
+++ subscriber-service/src/test/java/com/lsefiane/subscriber/SubscriberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsefiane.subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

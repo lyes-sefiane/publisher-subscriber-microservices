3610cd474ac7c3b2f0c951586b073d89db1dc663
lsefiane <lyes.sefiane@gmail.com>
2021-06-25 20:32:17 -0400

    ~ Publisher Service Module Setup

---
+++ publiser-service/src/main/java/com/lsefiane/publiser/PubliserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lsefiane.publiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubliserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubliserServiceApplication.class, args);
+	}
+
+}

ce720c3d0b707208743acc71e4291a2d9bed0708
lsefiane <lyes.sefiane@gmail.com>
2021-06-25 21:10:52 -0400

    ~ Publisher-Subscriber Common Setup

---
+++ publisher-subscriber-common/src/test/java/com/lsefiane/common/PublisherSubscriberCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsefiane.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherSubscriberCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
